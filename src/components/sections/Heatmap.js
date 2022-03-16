a80c75bd3c378e54147fa22d479784d447e0780a
jip120912 <jahidulku120912@gmail.com>
2022-03-16 12:18:03 +0600

    pull

--- src/components/sections/Heatmap.js
+++ src/components/sections/Heatmap.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ReactApexChart from 'react-apexcharts'
+import { AgChartsReact } from 'ag-charts-react';
+import data from '../../utils/heatMapData'
+
 
 class Heatmap extends React.Component {
 
@@ -7,89 +9,42 @@ class Heatmap extends React.Component {
     super(props);
 
     this.state = {
-    
-      series: [
-        {
-          data: []
-        }
-      ],
+      
       options: {
-        legend: {
-          show: false
-        },
-        chart: {
-          height: 500,
-          type: 'treemap'
-        },
-        title: {
-          text: ''
-        },
-        dataLabels: {
-          enabled: true,
-          style: {
-            fontSize: '10px',
+        type: 'hierarchy',
+
+        data ,
+        series: [
+          {
+            type: 'treemap',
+            labelKey: 'name',
+            sizeKey: 'size',
+            colorKey: 'color',
+            tooltip: {
+              renderer: (params) => {
+                return {
+                  content: `<b>Change</b>: ${params.datum.colorValue.toFixed(
+                    2
+                  )}%`,
+                };
+              },
+            },
           },
-          formatter: function(text, op) {
-            return [text, op.value]
-          },
-          offsetY: -5
-        },
-        plotOptions: {
-          treemap: {
-            enableShades: true,
-            shadeIntensity: 0.5,
-            reverseNegativeShade: true,
-            colorScale: {
-              ranges: [
-                {
-                  from: -10, 
-                  to: -5,
-                  color: '#ee4758'
-                },
-                {
-                  from: -5, 
-                  to: 0,
-                  color: '#a30000'
-                },
-          
-               
-                {
-                  from: 0.001,
-                  to: 5,
-                  color: '#0aa54a'
-                }
-              ]
-            }
-          }
-        }
+        ],
+
+        // title: {
+        //   text: 'S&P 500 index stocks categorized by sectors and industries.',
+        // },
+        // subtitle: {
+        //   text:
+        //     'Area represents market cap. Color represents change from the day before.',
+        // },
       },
-    
-    
     };
   }
 
-  componentDidMount(){
-    fetch('https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=9f8bf374d13311bf6527af0ea58ebdb6')
-    .then(res=>res.json())
-    .then(response => {
-      const newSeries = [];
-      const getData = response.map(d => {
-        return{
-          x:d.symbol,
-          y:d.change
-        }
-      })
-      newSeries.push({
-        data: getData,
-      });
-      this.setState({
-        series: newSeries,
-        options: {
-          ...this.state.options,
-        }
-      });
-    })
-  }
+
+  componentDidMount(){}
 
   render() {
 
@@ -100,9 +55,9 @@ class Heatmap extends React.Component {
                 <h3 style = {{marginBottom:'3px'}}>MARKET OVERVIEW</h3>
                 <p style = {{color:'#666666 '}}>Overview of the biggest market cap stocks in each sector.</p>
             </div>
-            <div id="chart">
-                  <ReactApexChart options={this.state.options} series={this.state.series} type="treemap"  height={500} />
-            </div>
+         
+                   <AgChartsReact options={this.state.options} id = 'chartreact' />
+          
         </div>
 </div>
 
